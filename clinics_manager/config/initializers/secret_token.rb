5c2ccb2358262b9f4dec1105d358e37122767881
Jiaxi Luo <jiaxiluo@umich.edu>
2014-12-06 13:50:36 -0500

    init rails project

---
+++ clinics_manager/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ClinicsManager::Application.config.secret_token = '6e3cedbdb6495e3f03c7080b3b04709452588cf3e11e543439965bb8d26422b94b81d2e54f4411430c90bd4777752076862da2925d4e48013acb99480a536749'
