df13a04f08fe73c0d32f3626eb46405e41049af1
hongdp <hongdp@umich.edu>
2014-12-10 20:54:58 -0500

    validates and tests

---
+++ clinics_manager/app/models/hours.rb
@@ -0,0 +1,8 @@
+class Hours < ActiveRecord::Base
+  validates :clinic_id, :presence => true
+  validates :hour_type, :presence => true
+  validates :dummy_hour_pk, :presence => true
+  
+  self.set_table_name "Hours"
+  self.primary_key = "clinic_id"
+end
