53d59fb91de47fa471c952cf0e0ab70f4c42083b
Jiaxi Luo <jiaxiluo@umich.edu>
2014-12-13 14:49:09 -0500

    Revert "validates and tests"
    This reverts commit df13a04f08fe73c0d32f3626eb46405e41049af1.

--- clinics_manager/app/models/hours.rb
+++
@@ -1,8 +0,0 @@
-class Hours < ActiveRecord::Base
-  validates :clinic_id, :presence => true
-  validates :hour_type, :presence => true
-  validates :dummy_hour_pk, :presence => true
-  
-  self.set_table_name "Hours"
-  self.primary_key = "clinic_id"
-end
