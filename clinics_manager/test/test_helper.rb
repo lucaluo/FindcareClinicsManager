2c2ad34c3c7dd295fcdf9ceca3e60c9d5f4dbe85
Jiaxi Luo <jiaxiluo@umich.edu>
2015-01-18 05:20:44 +0000

    Add fixture class hours

--- clinics_manager/test/test_helper.rb
+++ clinics_manager/test/test_helper.rb
@@ -8,6 +8,7 @@ class ActiveSupport::TestCase
   # Note: You'll currently still have to declare fixtures explicitly in integration tests
   # -- they do not yet inherit this setting
   set_fixture_class :clinics => Clinic
+  set_fixture_class :hours => Hours
   set_fixture_class :clinic_inserts => ClinicInsert
   set_fixture_class :clinic_insert_hours => ClinicInsertHour
   set_fixture_class :clinic_insert_services => ClinicInsertService
