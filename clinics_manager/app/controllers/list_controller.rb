cab068be111cdaf016ef86955e6cac53c388fab8
kinglouisxviii <renl@umich.edu>
2015-01-22 21:47:54 -0500

    approve => N, html fix

--- clinics_manager/app/controllers/list_controller.rb
+++ clinics_manager/app/controllers/list_controller.rb
@@ -1,11 +1,11 @@
 class ListController < ApplicationController
   def listNew
-  	clinic_update_id = ClinicInsert.select('clinic_id').where('update_status' => :UPDATE).map(&:'clinic_id').uniq
+  	clinic_update_id = ClinicInsert.select('clinic_id').where('update_status' => :UPDATE).where('approve' => 'N').map(&:'clinic_id').uniq
   	@clinic_update = Clinic.where('clinic_id' => clinic_update_id)
-  	clinic_delete_id = ClinicInsert.select('clinic_id').where('update_status' => :DELETE).map(&:'clinic_id').uniq
+  	clinic_delete_id = ClinicInsert.select('clinic_id').where('update_status' => :DELETE).where('approve' => 'N').map(&:'clinic_id').uniq
   	@clinic_delete = Clinic.where('clinic_id' => clinic_delete_id)
 
-  	@add_clinic_inserts = ClinicInsert.where('update_status' => :ADD)
+  	@add_clinic_inserts = ClinicInsert.where('update_status' => :ADD).where('approve' => 'N')
   end
 
   def listAll
