7084b3493781b6a6a748ebfb314fcbc1ef234a31
kinglouisxviii <renl@umich.edu>
2015-01-22 21:32:01 -0500

    listnew

--- clinics_manager/app/controllers/list_controller.rb
+++ clinics_manager/app/controllers/list_controller.rb
@@ -1,5 +1,10 @@
 class ListController < ApplicationController
   def listNew
-  	@clinic_inserts=Clinic_insert.all
+  	clinic_update_id = ClinicInsert.select('clinic_id').where('update_status' => :UPDATE).map(&:'clinic_id').uniq
+  	@clinic_update = Clinic.where('clinic_id' => clinic_update_id)
+  	clinic_delete_id = ClinicInsert.select('clinic_id').where('update_status' => :DELETE).map(&:'clinic_id').uniq
+  	@clinic_delete = Clinic.where('clinic_id' => clinic_delete_id)
+
+  	@add_clinic_inserts = ClinicInsert.where('update_status' => :ADD)
   end
 end
