e1dbc8f5accb1f61c404fad72d1a742af2ffe505
hongdp <hongdp@umich.edu>
2015-01-25 15:02:16 -0500

    combine views

--- clinics_manager/app/controllers/list_controller.rb
+++ clinics_manager/app/controllers/list_controller.rb
@@ -3,11 +3,13 @@ class ListController < ApplicationController
   	clinic_update_id = ClinicInsert.select('clinic_id').where('approve' => :N).where('update_status' => :UPDATE).map(&:'clinic_id').uniq
   	@clinic_update = Clinic.where('clinic_id' => clinic_update_id)
   	
-	#clinic_delete_id = ClinicInsert.select('clinic_id').where('update_status' => :DELETE).where('approve' => 'N').map(&:'clinic_id').uniq
+	  #clinic_delete_id = ClinicInsert.select('clinic_id').where('update_status' => :DELETE).where('approve' => 'N').map(&:'clinic_id').uniq
   	#@clinic_delete = Clinic.where('clinic_id' => clinic_delete_id)
 
   	#select from the table ClinicInsert all the clinics that added
   	@add_clinic_inserts = ClinicInsert.where('update_status' => :ADD).where('approve' => 'N')
+    @title = "List New"
+    @header = "List New Clinics"
     render "list/listAll"
   end
 
@@ -18,6 +20,9 @@ class ListController < ApplicationController
   	@clinic_delete = Clinic.where('clinic_id' => clinic_delete_id)
 
   	@add_clinic_inserts = ClinicInsert.where('update_status' => :ADD)
+    @title = "List New"
+    @header = "List New Clinics"
+    render "list/listAll"
   end
 
   def listApproved
@@ -27,7 +32,9 @@ class ListController < ApplicationController
   	@clinic_delete = Clinic.where('clinic_id' => clinic_delete_id)
 
   	@add_clinic_inserts = ClinicInsert.where('approve' => :Y).where('update_status' => :ADD)
-
+    @title = "List New"
+    @header = "List New Clinics"
+    render "list/listAll"
   end
 
   private
