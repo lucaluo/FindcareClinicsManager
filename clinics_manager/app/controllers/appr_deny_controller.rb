5d4a7e97de054bd5170801ea4e79c87d9d9a7b49
hangyumich <hangy@umich.edu>
2015-02-08 14:42:26 -0500

    appr_deny_controller

--- clinics_manager/app/controllers/appr_deny_controller.rb
+++ clinics_manager/app/controllers/appr_deny_controller.rb
@@ -1,11 +1,27 @@
 class ApprDenyController < ApplicationController
   def approve
-    @par = params[:clinic] 
+    @form_params = params[:clinic]
+    @clinic_insert = ClinicInsert.find(params[:transc_id])
+    if @clinic_insert.update_status == "UPDATE"
+      @clinic = Clinic.find(@clinic_insert[:clinic_id])
+      @clinic.update_attributes(@form_params)
+      @clinic.save
+      @clinic_inserts = ClinicInsert.where("clinic_id = ? AND approve = ?", @clinic.clinic_id, "N")
+      @clinic_inserts.each do |ci|
+        ci.approve = "Y"
+        ci.save
+      end
+    elsif @clinic_insert.update_status == "ADD"
+      @clinic = Clinic.new(@form_params)
+      @clinic.save
+      @clinic_inserts = ClinicInsert.where("clinic_id = ? AND approve = ?", @clinic.clinic_id, "N")
+      @clinic_inserts.each do |ci|
+        ci.approve = "Y"
+        ci.save
+      end
+    end
   end
 
-  # def form_params
-  #   params.require(:clinic_name)#.permit(:first_name, :last_name, :other_stuff)
-  # end
 
   def deny
     @clinic_insert = ClinicInsert.find(params[:transc_id])
@@ -21,5 +37,4 @@ class ApprDenyController < ApplicationController
       @clinic_insert.save
     end
   end
-
 end
